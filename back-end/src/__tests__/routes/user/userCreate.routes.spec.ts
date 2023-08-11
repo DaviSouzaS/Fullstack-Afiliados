5fd5cd530a74b6363faefffa42e5be2d196dfd0e
davi <daavisouza.santana@gmail.com>
2023-08-11 16:32:17 -0300

    feat: create integration tests for login

--- back-end/src/__tests__/routes/user/userCreate.routes.spec.ts
+++ back-end/src/__tests__/routes/user/userCreate.routes.spec.ts
@@ -4,19 +4,19 @@ import { hashSync } from "bcryptjs";
 import request from "supertest";
 import app from "../../../app";
 
-describe("Testing the user routes", () => {
-  let connection: DataSource;
+describe("Testing the user route", () => {
+  let connection: DataSource
 
   beforeAll(async () => {
     await AppDataSource.initialize()
       .then((res) => (connection = res))
       .catch((err) => {
-        console.error("Error during Data Source initialization", err);
+        console.error("Error during Data Source initialization", err)
       });
   });
 
   afterAll(async () => {
-    await connection.destroy();
+    await connection.destroy()
   });
 
   test("Should be able to create a new user", async () => {
@@ -84,6 +84,6 @@ describe("Testing the user routes", () => {
             ],
         }
       })        
-    );          
-  });
-});
+    )      
+  })
+})
